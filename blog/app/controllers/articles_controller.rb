ec76409e363499f7ad8e716be3e048dfbe5a5005
Bruno Fernandes <bruno.gabriel.fernandes@gmail.com>
2017-07-23 15:26:41 -0300

    Adding some form validation and error handling.

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -8,13 +8,17 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
  
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new' # not redirect, so it remains in the same view with the same data
+    end
   end
 
   private
